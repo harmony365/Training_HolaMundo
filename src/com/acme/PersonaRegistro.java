8748a3d210e220334d8fa648aa94ee4be5d38ce5
mramos <moisesiberico@gmail.com>
2023-09-30 21:19:27 +0200

    se remplasa las personas por registro persona

--- src/com/acme/PersonaRegistro.java
+++
@@ -1,8 +0,0 @@
-package com.acme;
-
-public record PersonaRegistro(String nombre, String apellido) {
-    @Override
-    public String toString() {
-        return "%s %s".formatted(nombre,apellido);
-    }
-}
