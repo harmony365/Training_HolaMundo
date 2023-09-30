dae1ee104db5b47ccc409971411d88252e57fea6
mramos <moisesiberico@gmail.com>
2023-09-30 18:57:02 +0200

    se agrego clase Mensaje

---
+++ src/com/acme/Mensaje.java
@@ -0,0 +1,16 @@
+package com.acme;
+import java.util.Scanner;
+
+public class Mensaje {
+    public String solicitaDatos(String mensaje){
+        System.out.print(mensaje);
+        Scanner scn = new Scanner(System.in);
+        String dato = scn.nextLine();
+        return dato;
+
+    }
+
+    public void saludar(String nombre){
+        System.out.printf("Hola %s, bienvenido a Java \n", nombre);
+    }
+}
