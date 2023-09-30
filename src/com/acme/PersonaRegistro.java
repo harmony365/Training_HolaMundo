cedac376cd66f57c06da849312567d582d6c8a24
mramos <moisesiberico@gmail.com>
2023-09-30 21:01:11 +0200

    se agrego una línea de código inservible

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -3,6 +3,7 @@ package com.acme;
 public record PersonaRegistro(String nombre, String apellido) {
     @Override
     public String toString() {
+        System.out.println("Esta instrucción no finciona");
         return "%s %s".formatted(nombre,apellido);
     }
 }
