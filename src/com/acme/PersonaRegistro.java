42bcbb7272ec6c540b022c43ad9c61ff7c89f95a
Moisés J. Ramos Z <99802333+harmony365@users.noreply.github.com>
2023-09-30 21:07:23 +0200

    Comentario sobre codigo inservible

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -3,7 +3,8 @@ package com.acme;
 public record PersonaRegistro(String nombre, String apellido) {
     @Override
     public String toString() {
-        System.out.println("Esta instrucción no finciona");
+        // Moises comente este cambio
+        //System.out.println("Esta instrucción no finciona");
         return "%s %s".formatted(nombre,apellido);
     }
 }
