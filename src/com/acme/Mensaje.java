97a3d7b152971fcf02bb74ea52e4dfa2be33e931
mramos <moisesiberico@gmail.com>
2023-09-30 18:57:55 +0200

    se agrego clase Mensaje

--- src/com/acme/Mensaje.java
+++ src/com/acme/Mensaje.java
@@ -5,8 +5,7 @@ public class Mensaje {
     public String solicitaDatos(String mensaje){
         System.out.print(mensaje);
         Scanner scn = new Scanner(System.in);
-        String dato = scn.nextLine();
-        return dato;
+        return scn.nextLine();
 
     }
 
